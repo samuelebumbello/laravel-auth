85b1894c9eeec20e971753f6d1272b8b3eb6a5f0
samuelebumbello <samuelebumbello17@gmail.com>
2023-06-29 23:41:48 +0200

    first commit

---
+++ config/projects.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+    [
+        "title"=> 'Boolflix',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=>date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'Boolzap',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'La Molisana',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'Laravel auth',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'progetto 5',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'progetto 6',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'progetto 7',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'progetto 8',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'progetto 9',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+    [
+        "title"=> 'progetto 10',
+        "type"=> 'Front end',
+        "thumb"=> 'image',
+        "date_creation"=> date_create_from_format('d/m/Y', '02/06/2023')->format('Y-m-d'),
+        "description"=> 'descrizione progetto',
+    ],
+
+    ];
---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container p-5">
+
+    @if (session('delete'))
+        <div class="alert alert-success" role="alert">
+            {{ session('delete') }}
+        </div>
+    @endif
+
+    <h4 class="mb-3">Elenco progetti</h4>
+
+    <a href="{{ route('admin.projects.create')}}" class="btn btn-primary mb-4">Inserisci nuovo progetto</a>
+
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col"><a href="{{ route('admin.orderby', ['direction'=> $direction]) }}">#ID</a></th>
+            <th scope="col">Titolo progetto</th>
+            <th scope="col">Tipologia</th>
+            <th scope="col">Data di creazione</th>
+            <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($projects as $project)
+            <tr>
+                <td>{{ $project->id }}</td>
+                <td>{{ $project->title }}</td>
+                <td>{{ $project->type }}</td>
+                <td>{{ $project->date_creation }}</td>
+                <td>
+                    <a href="{{ route('admin.projects.show' , $project) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
+                    <a href="{{ route('admin.projects.edit' , $project) }}" class="btn btn-warning"><i class="fa-solid fa-pen"></i></a>
+
+                    @include('admin.partials.form-delete')
+                </td>
+            </tr>
+            @endforeach
+
+        </tbody>
+        </table>
+
+        <div>
+            {{ $projects->links() }}
+        </div>
+
+</div>
+@endsection
